5ebc46b541cbf907b884700fcf460b5bdf306a5f
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-07-15 05:51:51 +0300

    Enumerates specific countries

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,13 @@
+-- Creates a table users following these requirements:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- country, enumeration of countries: US, CO and TN, never null
+-- (= default will be the first element of the enumeration, here US)
+
+CREATE TABLE IF NOT EXISTS users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US' 
+);
